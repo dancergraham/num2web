1719fe1eb012da74b2ebcf0728879c967395ef1b
dancergraham <graham.knapp@gmail.com>
2022-05-15 21:28:40 +0200

    formatting
    Signed-off-by: dancergraham <graham.knapp@gmail.com>

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import flask
 from flask import Flask, render_template, request
-from num2words import num2words, CONVERTER_CLASSES
+from num2words import CONVERTER_CLASSES, num2words
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("num2web.html",
-                           languages=list(CONVERTER_CLASSES.keys()),
-                           name="bob"
-                           )
+    return render_template(
+        "num2web.html", languages=list(CONVERTER_CLASSES.keys()), name="bob"
+    )
 
 
 @app.route("/num2words/", methods=["POST"])
 def print_number():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.json
         number = float(data["number"])
         language = data["language"]
