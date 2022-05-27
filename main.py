cc19fa44482c810aa1faaed3d68c2f563652021b
dancergraham <graham.knapp@gmail.com>
2022-05-27 11:38:17 +0200

    doc: Add docstrings
    Signed-off-by: dancergraham <graham.knapp@gmail.com>

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """Render the single page template"""
     return render_template(
-        "num2web.html", languages=list(CONVERTER_CLASSES.keys()), name="bob"
+        "num2web.html", languages=CONVERTER_CLASSES.keys(), name="bob"
     )
 
 
 @app.route("/num2words/", methods=["POST"])
 def print_number():
+    """Endpoint for the num2words service."""
     if request.method == "POST":
         data = request.json
         number = float(data["number"])
